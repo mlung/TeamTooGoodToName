8cc62ddd41fa934fb6aa9fb9373e7025bd81ce9f
Miranda Lung <mlung@purdue.edu>
2015-04-10 18:03:32 -0400

    Validation and Combo Box classes

---
+++ GroupProject/GroupProject/clsComboBoxItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class clsComboBoxItem
+{
+    private int aID;
+    private string aText;
+
+    public int ID
+    {
+        get { return aID; }
+        set { aID = value; }
+    }
+
+    public string Text
+    {
+        get { return aText; }
+        set { aText = value; }
+    }
+
+    public override string ToString()
+    {
+        return aText;
+    }
+}
