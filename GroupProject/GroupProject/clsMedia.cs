7c9282760e1aaf09d500d81f0bdfe3b5b114aaa5
Miranda Lung <mlung@purdue.edu>
2015-04-12 19:47:07 -0400

    okay!

---
+++ GroupProject/GroupProject/clsMedia.cs
@@ -0,0 +1,10 @@
+﻿//Miranda Lung, CNIT 255 Group Project
+//Last edited 4/12/2015
+
+public class clsMedia
+    //Class for media objects: DVDs, books, movies, etc.
+{
+    private int myID;
+    private string Title;
+    private 
+}
---
+++ GroupProject/GroupProject/clsSQL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Configuration; //Config file
+using System.Data.OleDb; //Database connectivity
+using System.Collections;
+
+public class clsSQL
+{
+    public static ArrayList queryVendor (string sql)
+    {
+        ArrayList vendors = new ArrayList();
+        return vendors;
+    }
+
+    private void openDatabaseConnection(OleDbConnection db)
+    {
+        //string connectionString = ConfigurationManager.AppSettings["DBConnectionString"] + gFilename;
+        //db = new OleDbConnection(connectionString);
+    }
+}
